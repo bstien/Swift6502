2461fd4cf86ff42d41feea8b3341d4cebb273a1e
Bastian Stien <bastian.stien@gmail.com>
2023-10-07 19:22:24 +0200

    Bus: Add init and make sure address exists before saving/fetching data

--- Sources/Swift6502/Bus.swift
+++ Sources/Swift6502/Bus.swift
@@ -1,13 +1,19 @@
 import Foundation
 
 class Bus {
-    var ram = Array(repeating: UInt8(0x00), count: Int(UInt16.max))
+    var ram: [UInt8]
+
+    init(ram: [UInt8] = Array(repeating: UInt8(0x00), count: Int(UInt16.max))) {
+        self.ram = ram
+    }
 
     func read(_ address: UInt16) -> UInt8 {
-        ram[Int(address)]
+        guard ram.indices.contains(Int(address)) else { return 0 }
+        return ram[Int(address)]
     }
 
     func write(_ address: UInt16, data: UInt8) {
+        guard ram.indices.contains(Int(address)) else { return }
         ram[Int(address)] = data
     }
 }
