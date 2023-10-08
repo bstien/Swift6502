77a093031eed2b15c79b3b9d7c4c64f9ce48f500
Bastian Stien <bastian.stien@gmail.com>
2023-10-08 18:45:43 +0200

    BPL: Update test method name

--- Tests/Swift6502Tests/Instructions/BPL_Tests.swift
+++ Tests/Swift6502Tests/Instructions/BPL_Tests.swift
@@ -23,7 +23,7 @@ class BPL_Tests: XCTestCase {
         expect(cpu.pc) == 0x02
     }
 
-    func test_it_continues_on_negative_not_set() {
+    func test_it_continues_on_negative_set() {
         let cpu = CPU.create(ram: [0x80])
         cpu.setupAddressing(using: .rel)
         cpu.setFlag(.negative, true)
