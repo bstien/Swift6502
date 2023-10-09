01f0f278ba5a01cbe5c8a1a462df220289ad04e1
Bastian Stien <bastian.stien@gmail.com>
2023-10-09 21:40:27 +0200

    Fix incorrect status bit positions

--- Tests/Swift6502Tests/StatusRegisterTests.swift
+++ Tests/Swift6502Tests/StatusRegisterTests.swift
@@ -13,7 +13,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.negative))
 
         cpu.setFlag(.negative, true)
-        XCTAssertEqual(0b0000_0001, cpu.flags)
+        XCTAssertEqual(0b1000_0000, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.negative))
 
         cpu.setFlag(.negative, false)
@@ -26,7 +26,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.overflow))
 
         cpu.setFlag(.overflow, true)
-        XCTAssertEqual(0b0000_0010, cpu.flags)
+        XCTAssertEqual(0b0100_0000, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.overflow))
 
         cpu.setFlag(.overflow, false)
@@ -39,7 +39,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.break))
 
         cpu.setFlag(.break, true)
-        XCTAssertEqual(0b0000_1000, cpu.flags)
+        XCTAssertEqual(0b0001_0000, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.break))
 
         cpu.setFlag(.break, false)
@@ -52,7 +52,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.decimal))
 
         cpu.setFlag(.decimal, true)
-        XCTAssertEqual(0b0001_0000, cpu.flags)
+        XCTAssertEqual(0b0000_1000, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.decimal))
 
         cpu.setFlag(.decimal, false)
@@ -65,7 +65,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.interrupt))
 
         cpu.setFlag(.interrupt, true)
-        XCTAssertEqual(0b0010_0000, cpu.flags)
+        XCTAssertEqual(0b0000_0100, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.interrupt))
 
         cpu.setFlag(.interrupt, false)
@@ -78,7 +78,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.zero))
 
         cpu.setFlag(.zero, true)
-        XCTAssertEqual(0b0100_0000, cpu.flags)
+        XCTAssertEqual(0b0000_0010, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.zero))
 
         cpu.setFlag(.zero, false)
@@ -91,7 +91,7 @@ final class StatusRegisterTests: XCTestCase {
         XCTAssertFalse(cpu.readFlag(.carry))
 
         cpu.setFlag(.carry, true)
-        XCTAssertEqual(0b1000_0000, cpu.flags)
+        XCTAssertEqual(0b0000_0001, cpu.flags)
         XCTAssertTrue(cpu.readFlag(.carry))
 
         cpu.setFlag(.carry, false)
