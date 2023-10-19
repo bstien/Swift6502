c9489bbe05e7dd86933098c6ace17f6ca25326b3
Bastian Stien <bastian.stien@gmail.com>
2023-10-19 22:19:11 +0200

    Add test for page crossing

--- Tests/Swift6502Tests/AddressModes/AddressMode_REL_Tests.swift
+++ Tests/Swift6502Tests/AddressModes/AddressMode_REL_Tests.swift
@@ -6,10 +6,11 @@ class AddressMode_REL_Tests: XCTestCase {
     func test_with_positive_offset() {
         let cpu = CPU.create(ram: [0x03])
 
-        cpu.setupAddressing(using: .rel)
+        let extraCycles = cpu.setupAddressing(using: .rel)
 
         expect(cpu.addressAbsolute) == 0x0004
         expect(cpu.pc) == 0x01
+        expect(extraCycles) == 1
     }
 
     func test_with_negative_offset() {
@@ -19,5 +20,15 @@ class AddressMode_REL_Tests: XCTestCase {
 
         expect(cpu.addressAbsolute) == 0xFF81
         expect(cpu.pc) == 0x01
+    }
+
+    func test_page_boundry_cross_produces_extra_cycles() {
+        let cpu = CPU.create(ram: [0x80])
+
+        let extraCycles = cpu.setupAddressing(using: .rel)
+
+        expect(cpu.addressAbsolute) == 0xFF81
+        expect(cpu.pc) == 0x01
+        expect(extraCycles) == 2
     }
 }
